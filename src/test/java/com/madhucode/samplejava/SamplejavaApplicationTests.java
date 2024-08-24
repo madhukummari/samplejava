92d9bb0e503722ce714df5aacdd933524f48353c
Madhu <madhu@Madhus-MacBook-Pro.local>
2024-08-24 09:46:36 +0530

    First commit

---
+++ src/test/java/com/madhucode/samplejava/SamplejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madhucode.samplejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
